d3272c30f4f503d237f295f626de45f36dd388e3
Koen van Dijk <kjw.vandijk@gmail.com>
2017-06-19 16:35:03 +0200

    new project

---
+++ lib/bpmnix.ex
@@ -0,0 +1,18 @@
+defmodule Bpmnix do
+  @moduledoc """
+  Documentation for Bpmnix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bpmnix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bpmnix_test.exs
@@ -0,0 +1,8 @@
+defmodule BpmnixTest do
+  use ExUnit.Case
+  doctest Bpmnix
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
