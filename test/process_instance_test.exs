2e46f3881a39ba2b4bfc6f1ae33fd01565af5bdd
Koen van Dijk <kjw.vandijk@gmail.com>
2017-08-23 20:11:37 +0200

    added some tests for the execution of steps

--- test/process_instance_test.exs
+++ test/process_instance_test.exs
@@ -5,32 +5,41 @@ defmodule ProcessInstanceTest do
 
 
 
-    setup do
-      instance = ProcessInstance.new_instance(1 , simple_process())
-      {:ok, instance: instance}
-    end
+  setup do
+    instance = ProcessInstance.new_instance(1, simple_process())
+    {:ok, instance: instance}
+  end
 
-    test "create a new instance", %{instance: instance} do
-        assert instance.id == 1
-        assert instance.history == []
-        assert instance.status == [{:event, :start}]
-        assert instance.process_definition.id == :simple_process
-    end
+  test "create a new instance", %{instance: instance} do
+    assert instance.id == 1
+    assert instance.history == []
+    assert instance.status == [{:event, :start}]
+    assert instance.process_definition.id == :simple_process
+  end
 
-    test "go to the next step", %{instance: instance} do
-        nextInstance = ProcessInstance.next_step(instance)
+  test "go to the next step", %{instance: instance} do
+    nextInstance = ProcessInstance.next_step(instance)
 
-        assert nextInstance.status == [task: :task1]
-        assert nextInstance.history == [event: :start]
-    end
+    assert nextInstance.status == [task: :task1]
+    assert nextInstance.history == [event: :start]
+  end
 
-#   FIXME might be implimented later
-#    test "complete a process", %{instance: instance} do
-#        refute instance
-#    end
-#
-#    test "store an error", %{instance: instance} do
-#        refute instance
-#    end
+
+
+  test "complete a process", %{instance: instance} do
+    assert instance.is_completed
+  end
+
+  test "store an error", %{instance: instance} do
+    assert length(instance.errors) > 0
+  end
+
+  test "execute a task", %{instance: instance} do
+    assert instance.task.completed > 0
+  end
+
+  test "collect step metrics", %{instance: instance} do
+    assert instance.task.endtime > 0
+  end
 
 end
